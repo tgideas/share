d4560f8f83235db79b3009abd539ea3fc7ed8464
mamboer <mamboer@live.com>
2014-10-28 00:49:58 +0800

    gg

--- grunt/concat_css.js
+++ grunt/concat_css.js
@@ -3,7 +3,8 @@ module.exports = {
         src: [
             '2014/mobi/css/base.css',
             '2014/mobi/css/slide.css',
-            '2014/mobi/css/style.css'
+            '2014/mobi/css/style.css',
+            '2014/mobi/css/fontello.css'
         ],
         dest: '2014/mobi/dist/css/all.css'
     }
